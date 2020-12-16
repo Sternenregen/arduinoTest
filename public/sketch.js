423d8a8fe602711c11db226258c9dca1c99257f1
Sternenregen <73792588+Sternenregen@users.noreply.github.com>
2020-12-16 16:26:01 +0100

    Add files via upload

---
+++ public/sketch.js
@@ -0,0 +1,50 @@
+// use Chrome Browser
+// enable the 'experimental-web-platform-features' flag opening 'chrome://flags'
+// serial magic happens here > "libraries/webserial.js" 
+// good documentation web serial API: https://web.dev/serial/
+
+
+let connectButton;
+let serialController;
+
+function setup() {
+  socket = io.connect();
+  socket.on('arduino', printMsg);
+
+  // canvas
+  canvas = createCanvas(640, 480).parent('canvas');
+
+  // init serial connection with baudrate
+  serialController = new SerialController(57600);
+
+  // init gui
+  connectButton = createButton("Initialize Serial Connection");
+  connectButton.class("button");
+  connectButton.mousePressed(initSerial);
+}
+
+function draw() {
+  // background
+  background(0);
+  textAlign(CENTER, CENTER);
+  // just if serial controller ready an there is data
+  if (serialController.read() > 0 && serialController.hasData()) {
+    console.log(serialController.read());
+    background(255,0,255);
+  }
+
+}
+
+function sendData() {
+  data = {msg: true}
+  socket.emit('arduino', data);
+}
+
+function printMsg(data) {
+  print("Touch");
+}
+
+// init serial connection
+function initSerial() {
+  serialController.init();
+}
